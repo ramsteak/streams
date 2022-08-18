3b0cda32f7167fc80bd61f6a2d9c8d3368db3163
Marco Stecca <marco.stecca99@gmail.com>
2022-08-18 11:18:35 +0200

    Added docstring for all methods

--- streams/main.py
+++ streams/main.py
@@ -71,7 +71,16 @@ class Stream(Generic[_T]):
     """
 
     def __init__(self, __iter: Iterable[_T], length: Len = None) -> None:
-        """"""
+        """The method generates a stream object from any iterable, consuming it
+        lazily when needed. The length parameter is to be set when the length
+        cannot be determined automatically (such as iterables without a __len__
+        method or methods where calling __len__ would consume them). Whenever
+        possible it is recommended to prefer using the default generators,
+        such as range and count.
+
+        >>> Stream([1,2,3,4,5]) -> <1,2,3,4,5>
+        >>> Stream.range(4)    -> <0,1,2,3>
+        >>> Stream.primes().limit(3) -> <2,3,5>"""
         self.__iter = __iter
         if length == None:
             try:
@@ -102,11 +111,18 @@ class Stream(Generic[_T]):
 
     @classmethod
     def range(cls, *args) -> Stream[int]:
+        """The method generates a stream from the python range class. It uses
+        the same arguments of the range object in standard python."""
         return cls(range(*args), Len.FIN)
 
     @overload
     @classmethod
     def counter(cls) -> Stream[int]:
+        ...
+
+    @overload
+    @classmethod
+    def counter(cls, start: int) -> Stream[int]:
         ...
 
     @overload
@@ -121,12 +137,16 @@ class Stream(Generic[_T]):
 
     @classmethod
     def counter(cls, *args) -> Stream[int]:
+        """The method generates an infinite counting stream using the count object
+        from itertools, using the same arguments."""
         return cls(count(*args), Len.INF)
 
     @classmethod
     def generate(
         cls, __func: Callable[[Any], _T], args: tuple = None, kwargs: dict = None
     ) -> Stream[_T]:
+        """The methods evaluates the passed function every time in order to generate
+        the stream. Arguments to the function can be passed via args and kwargs"""
         if args is None:
             args = ()
         if kwargs is None:
@@ -140,23 +160,37 @@ class Stream(Generic[_T]):
 
     @classmethod
     def repeat(cls, __val: _T) -> Stream[_T]:
+        """The method generates an infinite stream repeating the same object"""
         return cls.generate(lambda: __val)
 
     @classmethod
     def random(cls) -> Stream[float]:
+        """The method generates an infinite stream of random values between 0 and
+        1 evaluated via the random.random method"""
         return cls.generate(random)
 
     @classmethod
     def randint(cls, a: int, b: int) -> Stream[int]:
+        """The method generates an infinite stream of random integers between a
+        and b with the randint function"""
         return cls.generate(randint, (a, b))
 
     @classmethod
     def randbool(cls) -> Stream[bool]:
+        """The method generates an infinite stream of random bools"""
         return cls.generate(randint, (0, 1)).eval(bool)
 
     @classmethod
     def primes(cls) -> Stream[int]:
+        """The method generates an infinite stream of primes. The main focus of
+        the function is not storing the values to memory, relying on a partially
+        optimized method to generate and evaluating primes on the fly. It might be
+        slow for very large numbers."""
+
         def quasiprimes():
+            """The method partially optimizes the prime search because primes can
+            only be one greater or one smaller than 6. Therefore it is much faster
+            than checking every number or every second number."""
             yield 2
             yield 3
             n = 6
@@ -182,9 +216,18 @@ class Stream(Generic[_T]):
 
         return cls(loop(), Len.INF)
 
+    @classmethod
+    def empty(cls) -> Stream:
+        """The method generates an empty stream"""
+        return cls([], Len.FIN)
+
     # Operations to limit the data items
 
     def filter(self, __key: Callable[[_T], bool], keep_if: bool = True) -> Stream[_T]:
+        """The method iterates through the stream evaluating each element via the
+        __key function. All values which do not return the same value as keep_if
+        are discarded"""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -199,6 +242,8 @@ class Stream(Generic[_T]):
         return self
 
     def slice(self, start: int = 0, stop: int = None, step: int = 1) -> Stream[_T]:
+        """The method allows to select a slice of the stream via the start, stop,
+        step parameters."""
         if stop is not None:
             self.__length = Len.FIN
         if step < 1:
@@ -226,6 +271,9 @@ class Stream(Generic[_T]):
         return self
 
     def skip(self, count: int) -> Stream[_T]:
+        """The method allows to skip a number of items, resuming the stream after
+        the specified amount"""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -241,6 +289,7 @@ class Stream(Generic[_T]):
         return self
 
     def limit(self, count: int) -> Stream[_T]:
+        """The method cuts the stream after the specified amount of item is yielded"""
         self.__length = Len.FIN
 
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
@@ -260,6 +309,9 @@ class Stream(Generic[_T]):
         return self
 
     def stop(self, __when: Callable[[_T], bool], inclusive: bool = False) -> Stream[_T]:
+        """The method stops the stream when the __when function evaluates to true.
+        If set to true, the inclusive parameter prints the element that evaluated
+        to true before interrupting the stream."""
         if self.__length == Len.INF:
             self.__length = Len.UNK
 
@@ -280,6 +332,9 @@ class Stream(Generic[_T]):
         return self
 
     def distinct(self) -> Stream[_T]:
+        """The method removes all elements that repeat in the stream. It is to be
+        noted that it uses a set to cache the elements, therefore the elements must
+        be hashable"""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
 
@@ -301,6 +356,9 @@ class Stream(Generic[_T]):
     # Operations to change the data items
 
     def enumerate(self) -> Stream[_T]:
+        """The mehtod functions in the same way that the enumerate object functions
+        in normal python, by yielding tuples in the form of (index, value)."""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = enumerate(__iter)
@@ -316,6 +374,8 @@ class Stream(Generic[_T]):
         return self
 
     def replace(self, __old: _T, __new: _R) -> Stream[_T | _R]:
+        """The method replaces every instance of __old with __new"""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -331,6 +391,12 @@ class Stream(Generic[_T]):
     def replace_with(
         self, __with: Callable[[_T], _R], __when: Callable[[_T], bool] = lambda _: True
     ) -> Stream[_T | _R]:
+        """The method replaces every element with the output of the __with function.
+        If set, the __when function determines whether or not to change the element.
+        Exceptions thrown by either __with or __when are caught as StreamException,
+        and can be dealt with in a subsequent .exc method. Any exception that is not
+        caught in this way is raised in any subsequent method."""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -350,6 +416,14 @@ class Stream(Generic[_T]):
         return self
 
     def eval(self, __func: Callable[[_T], _R]) -> Stream[_R]:
+        """The method replaces every element of the stream with the output of the
+        __func function.
+        Exceptions thrown by __func are caught as StreamException, and can be dealt
+        with in a subsequent .exc method. Any exception that is not caught in this
+        way is raised in any subsequent method.
+        In order to catch BaseExceptions you can call the _eval method, but it is
+        not recommended to use it except in specific circumstances."""
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -406,6 +480,16 @@ class Stream(Generic[_T]):
         /,
         __with: _R | Callable[[_T], _R] = None,
     ) -> Stream[_T | _R]:
+        """The method allows to deal with Exceptions thrown by functions in the
+        stream.
+         - stop -> the stream is interrupted before the exception
+         - continue -> the element that raises the exception gets skipped
+         - replace -> the element is replaced by the item specified as __with
+         - with -> the element is replaced by the result of the __with function
+         with the original element (before the eval)"""
+        if todo not in ("stop", "continue", "with", "replace"):
+            raise ValueError
+
         def loop(__iter: Iterable[_T]) -> Iterable[_T]:
             try:
                 it = iter(__iter)
@@ -449,21 +533,28 @@ class Stream(Generic[_T]):
         yield from (e for e in self._loop() if __key(e))
 
     def list(self) -> list[_T]:
+        """The method consumes the stream and returns a list with the items of the
+        stream"""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return list(self._loop())
 
     def tuple(self) -> tuple[_T]:
+        """The method consumes the stream and returns a tuple with the items of the
+        stream"""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return tuple(self._loop())
 
     def set(self) -> set[_T]:
+        """The method consumes the stream and returns a set with the items of the
+        stream"""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return set(self._loop())
 
     def null(self) -> None:
+        """The method consumes the stream, returning nothing"""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         try:
@@ -473,9 +564,9 @@ class Stream(Generic[_T]):
         except StopIteration:
             return
 
-    def first(
-        self, __key: Callable[[_T], bool] = lambda _: True, default: _T = None
-    ) -> _T:
+    def first(self, __key: Callable[[_T], bool] = lambda _: True, default: _T = None) -> _T:
+        """The method consumes the first item of the stream, returning it. If the
+        stream is empty the default item is returned."""
         try:
             it = iter(self._loop_key(__key))
             return next(it)
@@ -485,6 +576,8 @@ class Stream(Generic[_T]):
     def last(
         self, __key: Callable[[_T], bool] = lambda _: True, default: _T = None
     ) -> _T:
+        """The method consumes the entire stream, returning only the last item.
+        If the stream is empty the default item is returned."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         try:
@@ -496,32 +589,40 @@ class Stream(Generic[_T]):
             return nx
 
     def sum(self, __key: Callable[[_T], bool] = lambda _: True) -> _T:
+        """The method consumes the stream with the sum method, returning the result."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return sum(e for e in self._loop() if __key(e))
 
     def count(self, __key: Callable[[_T], bool] = lambda _: True) -> _T:
+        """The method consumes the stream with the len method, returning the result."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return sum(1 for e in self._loop() if __key(e))
 
     def mean(self, __key: Callable[[_T], bool] = lambda _: True) -> _T:
+        """The method consumes the stream with the statistics.mean method,
+        returning the result."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return mean(e for e in self._loop() if __key(e))
 
     def all(self, __func: Callable[[_T], bool] = lambda x: x) -> bool:
+        """The method consumes the stream with the all method, returning the result."""
         return all(self._loop())
 
     def any(self, __func: Callable[[_T], bool] = lambda x: x) -> bool:
+        """The method consumes the stream with the any method, returning the result."""
         return any(self._loop())
 
     def min(self) -> _T:
+        """The method consumes the stream with the min method, returning the result."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return min(self._loop())
 
     def max(self) -> _T:
+        """The method consumes the stream with the max method, returning the result."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         return max(self._loop())
@@ -568,6 +669,10 @@ class Stream(Generic[_T]):
     # Operations to output to text
 
     def print(self, __format_spec: str = "<{}(, )>\n") -> Stream[_T]:
+        """The method prints the stream by piecing together each element. The method
+        does not consume by itself the stream, and it relies on other methods to
+        do it, printing only when a StopIteration or GeneratorExit exception is
+        raised."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
 
@@ -596,6 +701,8 @@ class Stream(Generic[_T]):
         return self
 
     def __format__(self, __format_spec: str) -> str:
+        """The method returns a string representation of the stream by consuming
+        it."""
         if self.__length == Len.INF:
             raise UnlimitedStreamException
         if __format_spec == "":
@@ -604,14 +711,26 @@ class Stream(Generic[_T]):
         return f"{pre}{sep.join(frm.format(e) for e in self)}{end}"
 
     def __repr__(self) -> str:
+        """The method implements the default python repr function"""
         return self.__format__(DEFAULT_REPR_FORMAT)
 
     def __str__(self) -> str:
+        """The method implements the default python str function"""
         return self.__format__(DEFAULT_FORMAT)
 
     # Miscellaneous
 
     def _length_override(self, length: Len) -> Stream[_T]:
+        """The method (normally not to be used) alters the believed length of the
+        generator. Some methods might raise UnlimitedStreamException if the stream
+        is marked as infinite. If for whatever reason you want to deal with infinity
+        or the stream is wrongfully marked you can set the length with the Len enum
+        or manually setting to:
+         - length = 1  : finite length
+         - length = 0  : unknown length
+         - length = -1 : infinite length"""
+        if length not in (1, 0, -1):
+            raise ValueError
         self.__length = length
         return self
 
