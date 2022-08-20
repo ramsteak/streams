6490a7a2d61eba3490e415bd02df942f2a6e45c8
Marco Stecca <marco.stecca99@gmail.com>
2022-08-20 20:25:34 +0200

    Correction to setup

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-# import setuptools
-
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
-# setuptools.setup(
-#     name="streams",
-#     version="1.0.0",
-#     author="ramsteak",
-#     author_email="ramsteak.git@gmail.com",
-#     description="Package to handle streams with dot notation",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     packages=setuptools.find_packages(),
-#     classifiers=[
-#         "Programming Language :: Python :: 3",
-#         "License :: OSI Approved :: GPLv3 License",
-#         "Operating System :: OS Independent",
-#     ],
-#     python_requires=">=3.7",
-# )
 import setuptools
 
 with open("README.md", "r") as fh:
