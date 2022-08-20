20ee43ad8aee5b5cbe337f47383f34e97a2ab63e
Marco Stecca <marco.stecca99@gmail.com>
2022-08-20 12:31:04 +0200

    Added docstring to exceptions

--- streams/exceptions.py
+++ streams/exceptions.py
@@ -5,6 +5,10 @@ _T = TypeVar("_T")
 
 
 class StreamException:
+    """Container class to catch and transport exceptions without raising them
+    immediately, which would interrupt the stream.
+    This class should not be used in any way outside of streams exception handling."""
+
     def __init__(self, exc: Exception, val: _T) -> None:
         self.exc, self.val = exc, val
 
@@ -13,8 +17,9 @@ class StreamException:
 
 
 class StreamFormatError(Exception):
-    ...
+    """Raised if the stream format does not follow the pattern"""
 
 
 class UnlimitedStreamException(Exception):
-    ...
+    """Raised by some methods that would hang given an infinite stream (cache,
+    reverse, last, ...)"""
